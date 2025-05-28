08a00c8b9eacb6db11a26627819e827d16f25798
xbvuno <cerrabruno.ist@gmail.com>
2025-05-28 16:35:51 +0200

    dual deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Silicon_Valley_WEB/',
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
 })
